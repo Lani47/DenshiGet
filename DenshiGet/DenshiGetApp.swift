27e6661c4167b3cd224daf8871d153cbb8436383
LaniLaniLani47 <21cm0147@jec.ac.jp>
2022-05-13 09:42:31 +0900

    Initial Commit

---
+++ DenshiGet/DenshiGetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenshiGetApp.swift
+//  DenshiGet
+//
+//  Created by cmStudent on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct DenshiGetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
